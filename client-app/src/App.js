cb75e111dbb782452e00d17b5d1f6e5d34f40a0e
AshishVerma18 <aashish.640@gmail.com>
2021-04-14 15:20:51 +0530

    first

---
+++ client-app/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <navbar className="navbar bg-dark">
+      <a  className="navbar-brand" href="/">ProCart</a>
+    </navbar>
+    </>
+        
+  );
+}
+
+export default App;
